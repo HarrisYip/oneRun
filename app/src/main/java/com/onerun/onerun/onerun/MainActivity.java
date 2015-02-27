f13bdb18c71cd7b59421db2d4f3711428719d465
Terrence <tjfleung@live.ca>
2015-02-27 18:59:33 -0500

    UI of Workout Set Screen

--- app/src/main/java/com/onerun/onerun/onerun/MainActivity.java
+++ app/src/main/java/com/onerun/onerun/onerun/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends ActionBarActivity
         switch(position) {
             case 0:
                 fragmentManager.beginTransaction()
-                        .replace(R.id.container,     PlaceholderFragment.newInstance(position + 1))
+                        .replace(R.id.container,     WorkoutSetFragment.newInstance(position + 1))
                         .commit();
                 break;
             case 1:

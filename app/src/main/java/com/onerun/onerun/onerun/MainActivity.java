64ebea7da51997822e4b0cab1488ddb6a4fe6bb1
Jason Jongseok Park <jasonpa90@gmail.com>
2015-02-28 22:53:03 -0500

    JP - fixed library issues on android.content.Context

--- app/src/main/java/com/onerun/onerun/onerun/MainActivity.java
+++ app/src/main/java/com/onerun/onerun/onerun/MainActivity.java
@@ -1,6 +1,7 @@
 package com.onerun.onerun.onerun;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
